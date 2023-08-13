f2b8050897680c0be2ed3fa7d66e264dc882e30f
Joseph Scott <joseph@josephscott.org>
2023-08-12 21:47:06 -0600

    Note the reference for the list of codes

--- src/console.php
+++ src/console.php
@@ -2,6 +2,8 @@
 declare( strict_types = 1 );
 namespace ASL;
 
+// List of escape codes
+// https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 class Console {
 	public function clear(): void {
 		echo "\x1b[2J";
